3179cfc8121a699ffbd31f4d3842e1d389a0f19b
Ardi <85466120+ardi10-dev@users.noreply.github.com>
2021-06-08 22:24:38 +0700

    Update home.php

--- View/home.php
+++ View/home.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
+<head>A
+	
+	
+	//ini saya hanya ngubah dan tambah komen
+	
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Zipposter</title>
